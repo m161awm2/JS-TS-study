3dbcc3b4fc15b22b4d421275accd1e2fa1fc3233
구본무 <kubonmo519@gmail.com>
2026-04-12 22:27:26 +0900

    Update Day02

---
+++ Day02/app.js
@@ -0,0 +1,9 @@
+document.getElementById("btn01").addEventListener("click", function() {
+    // 버튼을 클릭했을 때, 아래의 함수를 실행하라는 의미입니다.
+    document.getElementById("text01").innerText = "반갑습니다";
+}); // 버튼을 클릭했을 때, 제목의 글자가 "반갑습니다"로 바뀌도록 하는 코드입니다.
+
+document.getElementById("btn02").addEventListener("click", function() {
+    // 버튼을 클릭했을 때, 아래의 함수를 실행하라는 의미입니다.
+    document.getElementById("text02").innerText = "반갑습니다";
+}); // 버튼을 클릭했을 때, 제목의 글자가 "반갑습니다"로 바뀌도록 하는 코드입니다.
---
+++ Day02/app04.js
@@ -0,0 +1,12 @@
+document.getElementById("button").addEventListener("click",function(){
+    const name = document.getElementById("input_name").value;
+    const age = document.getElementById("input_age").value;
+    if (name.trim() === "" || age.trim() === ""){
+        document.getElementById("text_name").innerText = "값을 넣으세요!"
+        document.getElementById("text_age").innerText = "값을 넣으세요!"
+    }
+    else{
+    document.getElementById("text_name").innerText = name
+    document.getElementById("text_age").innerText = age
+    }
+});
