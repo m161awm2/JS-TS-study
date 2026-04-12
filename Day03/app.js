04f3ed794a9ae20bcf517b459821c27c5c46d0b7
구본무 <kubonmo519@gmail.com>
2026-04-13 01:03:17 +0900

    Update Day03

---
+++ Day03/app.js
@@ -0,0 +1,26 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("input_name").value;
+    const age = document.getElementById("input_age").value;
+    const content = document.getElementById("input_content").value;
+
+    if (name.trim().length < 2) {
+        alert("이름을 두 글자 이상 입력하세요");
+        return;
+    }
+
+    if (isNaN(age) || age.trim() === "") {
+        alert("나이는 숫자를 입력하세요!");
+        return;
+    }
+
+    if (content.trim().length < 5) {
+        alert("내용은 5글자 이상 입력하세요");
+        return;
+    }
+
+    document.getElementById("text_name").innerText = name;
+    document.getElementById("text_age").innerText = age;
+    document.getElementById("text_content").innerText = content;
+});
