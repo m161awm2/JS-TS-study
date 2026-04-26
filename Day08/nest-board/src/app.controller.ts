a9971e80bafd4f01102884c9e1b4959cf80e8f47
구본무 <kubonmo519@gmail.com>
2026-04-26 17:45:18 +0900

    commit

--- Day08/nest-board/src/app.controller.ts
+++ Day08/nest-board/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+posts = [
+  {
+    id: 1,
+    title:'첫째 제목',
+  },
+  {
+    id: 2,
+    title:'둘째 제목',
+  },
+  {
+    id: 3,
+    title:'셋째 제목',
   }
+]
+@Get()
+함수명(): string{
+	return "아무글";
 }
+@Get('post/:post_id')
+함수명2(@Param('post_id') post_id: string){
+  const postId = Number(post_id);
+  const post = this.posts.find((post) => post.id === postId);
+
+  return post;
+}
+
+
+}
+ 
