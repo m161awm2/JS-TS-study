f7af173a3b3f30629a3daa6ba720e7ad41ecdd01
구본무 <kubonmo519@gmail.com>
2026-04-28 19:07:25 +0900

    Day10

---
+++ Day10/board/src/app.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Body, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import mysql from "mysql2/promise";
+
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASSWORD = "Zdzdsmsm44!";
+const DB_NAME = "Day10";
+const db = mysql.createPool({
+  host:DB_HOST,
+  user:DB_USER,
+  password:DB_PASSWORD,
+  database:DB_NAME
+});
+async function init_db(){
+  const tempDb = await mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+  });
+  await tempDb.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+  await tempDb.end();
+
+  await db.query(`CREATE TABLE IF NOT EXISTS posts(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nickname TEXT,
+    title VARCHAR(100),
+    content TEXT
+    )`);
+}
+init_db();
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  @Post('api/write')
+  async write(@Body() body:any){
+    await db.query('INSERT INTO posts (nickname, title, content) VALUES (?,?,?)',[body.nickname,body.title,body.content]);
+    return "저장됨";
+  }
+  @Get('posts')
+  async postList() {
+    const [posts] = await db.query('SELECT * FROM posts ORDER BY id DESC');
+    return posts;
+  }
+
+}
